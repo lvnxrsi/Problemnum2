33c26e1ad98efa47489046398d7ec645b4d8a49d
lvnxrsi <luvnaarsi17@gmail.com>
2024-08-21 12:32:56 +0800

    Practiceprob2

---
+++ PracticeSets_02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSets_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Write a program that prints all the even numbers from 1 to 20 using a for loop.
+             */
+
+            int num1 = 21;
+            for (int i = 2; i < num1; i += 2)
+            { Console.WriteLine(i); }
+        }
+    }
+}
