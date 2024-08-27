a54ab0da0f20f99c7a4572b82f1605fa1d1d82c3
Luv <luvnaarsi17@gmail.com>
2024-08-27 22:42:02 +0800

    ps02_03
    ps02_03

---
+++ PSO2_03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PSO2_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Single Letter:");
+            char letter = Console.ReadLine()[0];
+
+
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+            {
+                Console.WriteLine($"Letter {letter} is a vowel.");
+
+            }
+
+            else if (char.IsLetter(letter))
+            {
+                Console.WriteLine($"Letter {letter} is a consonant.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid. Please Enter A Single Letter Only.");
+            }
+        }
+    }
+}
