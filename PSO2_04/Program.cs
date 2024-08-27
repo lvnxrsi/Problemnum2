888bd2acd7b29ea0ed0b4aa33be6d64ead952af1
Luv <luvnaarsi17@gmail.com>
2024-08-27 22:43:15 +0800

    ps02_04
    ps02_04

---
+++ PSO2_04/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PSO2_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any words:");
+            string word = Console.ReadLine();
+            string check = new string(word.Reverse().ToArray());
+
+            if (word == check)
+            {
+                Console.WriteLine($"The word {word} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"The word {word} is not a palindrome.");
+            }
+        }
+    }
+}
