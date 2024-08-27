dc4a7fe6488e84024f524ee4d00403dc14a09aa9
Luv <luvnaarsi17@gmail.com>
2024-08-27 22:53:40 +0800

    ps02_05
    ps02_05

---
+++ PSO2_05/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PSO2_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a Word: ");
+            string word = Console.ReadLine();
+
+            int[] number = new int[256];
+
+            foreach (char c in word)
+            {
+                number[c]++;
+            }
+
+
+            Console.WriteLine("Number of Repetitions:");
+            for (int x = 0; x < number.Length; x++)
+            {
+                if (number[x] > 0)
+                {
+                    Console.WriteLine($"{(char)x}: {number[x]}");
+                }
+            }
+        }
+    }
+}
+
